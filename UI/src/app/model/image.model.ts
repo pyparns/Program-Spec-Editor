731aae663ec4e532d8e9ef4904d2ebb5231a13cb
pyparns <piyaparn40541@gmail.com>
2022-09-14 09:28:41 +0700

    add image_description

--- UI/src/app/model/image.model.ts
+++ UI/src/app/model/image.model.ts
@@ -3,6 +3,7 @@ import { Component } from "./component.model";
 
 export interface Image {
     imageName: string;
+    imageDescription: string;
     components: Component[];
     actions: Action[];
 }
