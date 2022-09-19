44dc8b5231ae32ebe1419831be6d29ca3e07f01f
pyparns <piyaparn40541@gmail.com>
2022-09-19 10:32:29 +0700

    account service

---
+++ UI/src/app/service/account.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from '../model/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private userSubject: BehaviorSubject<User>;
+  public user: Observable<User>;
+
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) {
+    this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user')));
+    this.user = this.userSubject.asObservable();
+  }
+}
