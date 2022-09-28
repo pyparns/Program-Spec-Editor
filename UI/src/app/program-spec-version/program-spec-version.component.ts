7e8d8e89390086d34b18c70e287d5776507f50b4
pyparns <piyaparn40541@gmail.com>
2022-09-28 10:35:11 +0700

    edit: program spec page

--- UI/src/app/program-spec-version/program-spec-version.component.ts
+++ UI/src/app/program-spec-version/program-spec-version.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgramSpecVersionComponent implements OnInit {
 
+  date: any[] = [
+    {name: "Program Spec", date: "sep 13, 2022", version: "1"},
+    {name: "Program Spec", date: "sep 20, 2022", version: "2"},
+    {name: "Program Spec Editor", date: "sep 22, 2022", version: "3"},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
